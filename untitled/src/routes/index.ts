1be2186a2f7fd25afcc68077b419c9026e2e755d
Geoffrey Harper <geoffreyharper@ugls3.ece.uvic.ca>
2019-10-02 13:25:11 -0700

    project upload

---
+++ untitled/src/routes/index.ts
@@ -0,0 +1,66 @@
+
+import { NextFunction, Request, Response, Router } from "express";
+import { BaseRoute } from "./routes";
+import {HeroRouter} from "./heroRouter";
+
+
+/**
+ * / route
+ *
+ * @class IndexRoute
+ */
+export class IndexRoute extends BaseRoute {
+
+    /**
+     * Create the routes.
+     *
+     * @class IndexRoute
+     * @method create
+     * @static
+     */
+    public static create(router: Router) {
+        //log
+        console.log("[IndexRoute::create] Creating index route.");
+
+        //add home page route
+        router.get("/", (req: Request, res: Response, next: NextFunction) => {
+            new IndexRoute().index(req, res, next);
+        });
+
+
+    }
+
+
+    /**
+     * Constructor
+     *
+     * @class IndexRoute
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * The home page route.
+     *
+     * @class IndexRoute
+     * @method index
+     * @param req {Request} The express Request object.
+     * @param res {Response} The express Response object.
+     * @next {NextFunction} Execute the next method.
+     */
+    public index(req: Request, res: Response, next: NextFunction) {
+        //set custom title
+        this.title = "Home | This is the main page";
+        this.coursestask = ['a', 'b'];
+
+        //set message
+        let options: Object = {
+            "message": "Welcome to the UVic 350!"
+        };
+
+        //render template
+        this.render(req, res, "index", options);
+    }
+}
